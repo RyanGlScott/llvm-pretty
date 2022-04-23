74976cc3231bcde49bea22063acdb94b04e02617
Ryan Scott <ryan.gl.scott@gmail.com>
2022-04-23 17:11:57 -0400

    Draft: DIArgList
    TODO RGS: Description
    
    [ci skip]

--- src/Text/LLVM/AST.hs
+++ src/Text/LLVM/AST.hs
@@ -1130,6 +1130,7 @@ data DebugInfo' lab
   | DebugInfoTemplateValueParameter (DITemplateValueParameter' lab)
   | DebugInfoImportedEntity (DIImportedEntity' lab)
   | DebugInfoLabel (DILabel' lab)
+  | DebugInfoArgList (DIArgList' lab)
     deriving (Data, Eq, Functor, Generic, Generic1, Ord, Show, Typeable)
 
 type DebugInfo = DebugInfo' BlockLabel
@@ -1362,6 +1363,12 @@ data DISubroutineType' lab = DISubroutineType
   } deriving (Data, Eq, Functor, Generic, Generic1, Ord, Show, Typeable)
 
 type DISubroutineType = DISubroutineType' BlockLabel
+
+newtype DIArgList' lab = DIArgList
+  { dialArgs :: [ValMd' lab]
+  } deriving (Data, Eq, Functor, Generic, Generic1, Ord, Show, Typeable)
+
+type DIArgList = DIArgList' BlockLabel
 
 -- Aggregate Utilities ---------------------------------------------------------
 
